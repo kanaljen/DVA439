6fa95965c84ba69664e3ac7d666701d7e57f7d59
Kanaljen <22039515+kanaljen@users.noreply.github.com>
2019-03-06 11:40:32 +0100

    vectorplot.py is complete

---
+++ plots.py
@@ -0,0 +1,9 @@
+from vectorplot import plotAsVectors
+
+xlabel = 'sweden'
+ylabel = 'population'
+D1 = [0.33,0.25]
+D2 = [0,0.24]
+Q = [0.5,0.5]
+
+plotAsVectors(D1,D2,query=Q)
---
+++ vectorplot.py
@@ -0,0 +1,30 @@
+from matplotlib import pyplot as plt
+import time
+import numpy as np
+
+
+def plotAsVectors(x,y,query=[],labelx='',labely=''):
+    """ Creates a plot with vectors in the range [0,1] from x and y,
+        also optionally a query, and axes labels"""
+    zero = np.zeros(len(x))
+    plt.quiver(zero, zero, x, y, angles='xy', scale_units='xy', scale=1)
+    for i in range(0, len(x)):
+        xy = tuple([x[i], y[i]+0.03])
+        plt.annotate('d {} ({:3},{:3})'.format(i, x[i], y[i]),
+                     xy=xy, textcoords='data',
+                     horizontalalignment='center',
+                     fontsize=10)
+    if len(query) == 2:
+        plt.quiver(0, 0, query[0], query[1], angles='xy', scale_units='xy', scale=1)
+        xy = tuple([query[0], query[1] + 0.03])
+        plt.annotate('query ({:3},{:3})'.format(query[0], query[1]),
+                     xy=xy, textcoords='data',
+                     horizontalalignment='center',
+                     fontsize=10)
+    plt.xlabel = labelx
+    plt.ylabel = labely
+    plt.xlim(-0.1, 1.1)
+    plt.ylim(-0.1, 1.1)
+    plt.axhline(0, color='black',linestyle='dotted')
+    plt.axvline(0, color='black',linestyle='dotted')
+    plt.savefig('vectorplot({}).png'.format(time.strftime("%H:%M:%S")))
